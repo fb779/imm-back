2f51fb01d7164a2551c7f67971f952d939a57045
Fabian Forero <fabianforero779@gmail.com>
2020-10-29 15:25:02 -0400

    Ajuste de fecha modelo web-chat
    Se realiza ajuste de fecha para el guardado en base de datos

--- src/model/web-chat.model.js
+++ src/model/web-chat.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const moment = require('moment');
 
 const uniqueValidator = require('mongoose-unique-validator');
 
@@ -13,14 +12,14 @@ const Schema = mongoose.Schema;
  * - date: Date
  */
 
-const WebChatSchema = new Schema({
-  process: { type: Schema.Types.ObjectId, ref: 'Process', required: true },
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  message: { type: String, required: [true, 'the message is required'] },
-  date: { type: Date, default: moment() },
-  // date: { type: Date, default: Date.now() },
-}, { timestamps: true, collection: 'webchat' });
-
-
+const WebChatSchema = new Schema(
+  {
+    process: {type: Schema.Types.ObjectId, ref: 'Process', required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    message: {type: String, required: [true, 'the message is required']},
+    date: {type: Date, default: Date.now},
+  },
+  {timestamps: true, collection: 'webchat'}
+);
 
 module.exports = mongoose.model('WebChat', WebChatSchema);
